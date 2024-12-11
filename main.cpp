496d5200b2288c3a2bcb3a47900d7af4e9a25297
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-12-10 19:52:33 -0500

    i got my grub, but didn't pig out

--- main.cpp
+++ main.cpp
@@ -1,15 +1,71 @@
-#include <iostream>
-#include <list>
 #include <map>
-using namespace std;
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
 
-int main() {
+std::string gpaConverter(int type, float gpa) {
+    if (type == 4) {
+        std::map<float, float> conversionTable;
+        conversionTable[100] = 4.0;
+        conversionTable[93] = 4.0;
+        conversionTable[90] = 3.7;
+        conversionTable[87] = 3.3;
+        conversionTable[83] = 3.0;
+        conversionTable[80] = 2.7;
+        conversionTable[77] = 2.3;
+        conversionTable[73] = 2.0;
+        conversionTable[70] = 1.7;
+        conversionTable[67] = 1.3;
+        conversionTable[63] = 1.0;
+        conversionTable[60] = 0.7;
+        conversionTable[57] = 0.3;
+        conversionTable[53] = 0.0;
+        if (conversionTable[gpa]) {
+            std::ostringstream result;
+            result << "The 4 point scale GPA for " << gpa << " is " << conversionTable[gpa] << std::endl;
+            return result.str();
+        }
+        else {
+            float prevGPA = 0.0;
+            float prevValue = -1;
 
+            for (const auto& pair : conversionTable) {
+                float currentGPA = pair.first;
+                float currentValue = pair.second;
+                if (gpa <= prevGPA && gpa > currentGPA) {
+                    std::ostringstream result;
+                    result << "The 4 point scale GPA for " << gpa << " is " << prevValue << std::endl;
+                    return result.str();
+                }
+                prevGPA = currentGPA;
+                prevValue = currentValue;
+
+                return "GPA not found";
+            }
+        }
+    }
 }
 
-float gpaConverter(int type, float gpa) {
-    if (type == 4) {
-        map<float, float> conversionTable;
-        conversionTable
+int main() {
+    std::cout << "Welcome to GPA Converter!" << std::endl << "Convert from 100-pt to 4-pt (4) or from 4-pt to 100-pt (100)? ";
+    std::string choice;
+    std::cin >> choice;
+
+    if (choice == "4") {
+        std::cout << "Enter your GPA: ";
+        float gpa;
+        std::cin >> gpa;
+        std::cout << gpaConverter(4, gpa);
     }
+    else if (choice == "100") {
+        std::cout << "Enter your 4-pt GPA: ";
+        float gpa;
+        std::cin >> gpa;
+        std::cout << gpaConverter(100, gpa);
+    }
+    else {
+        std::cout << "Invalid choice. Please try again." << std::system("clear");
+        main();
+    }
+    return 0;
 }
