f31f8e6674108bd500ce8aa65b6837c575caf7e8
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-12-10 19:19:18 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <list>
+#include <map>
+using namespace std;
+
+int main() {
+
+}
+
+float gpaConverter(int type, float gpa) {
+    if (type == 4) {
+        map<float, float> conversionTable;
+        conversionTable
+    }
+}
